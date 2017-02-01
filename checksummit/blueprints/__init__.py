8d43bfb1d6032156f795c680c5975a4a6eeace40
Brian Balsamo <balsamo@uchicago.edu>
2017-01-31 22:12:26 -0600

    Initial commit

---
+++ checksummit/blueprints/__init__.py
@@ -0,0 +1,52 @@
+from hashlib import new, algorithms_available
+
+from flask import Blueprint, request
+from flask_restful import Resource, Api, reqparse
+
+from nothashes import crc32, adler32
+
+additional_algos = ["crc32", "adler32"]
+additional_algos = set(additional_algos)
+disallowed_algos = []
+disallowed_algos = set(disallowed_algos)
+
+
+blueprint = Blueprint('checksummit', __name__)
+
+api = Api(blueprint)
+
+
+def produce_checksums(f, hashers):
+    data = f.read()
+    while data:
+        for x in hashers:
+            x.update(data)
+        data = f.read()
+    return {x.name: x.hexdigest() for x in hashers}
+
+
+class FileIn(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('hash', action="append")
+        args = parser.parse_args()
+        hashers = []
+        for x in args['hash']:
+            if x in disallowed_algos:
+                return {"message": "Disallowed algorithm included. ({})".format(x)}
+            elif x not in algorithms_available and x not in additional_algos:
+                return {"message": "Algorithm not supported. ({})".format(x)}
+            elif x == "crc32":
+                hashers.append(crc32())
+            elif x == "adler32":
+                hashers.append(adler32())
+            else:
+                hashers.append(new(x))
+
+        if len(request.files) < 1:
+                return {"message": "No file(s) detected."}
+
+        return {request.files[x].name: produce_checksums(request.files[x], hashers) for x in request.files}
+
+
+api.add_resource(FileIn, '/')
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open("README.md", 'r') as f:
+        return f.read()
+
+setup(
+    name = "checksummit",
+    description = "A REST API Microservice for computing checksums",
+    long_description = readme(),
+    packages = find_packages(
+        exclude = [
+        ]
+    ),
+    install_requires = [
+        'flask',
+        'flask_restful',
+        'nothashes'
+    ],
+    dependency_links = [
+        'https://github.com/bnbalsamo/nothashes' +
+        '/tarball/master#egg=nothashes'
+    ]
+)
