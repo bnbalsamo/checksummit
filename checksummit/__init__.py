aad81a408792cac5cb44297451d0fa07b5f9e00b
Brian Balsamo <balsamo@uchicago.edu>
2017-02-19 23:47:03 -0600

    Added the ability to set a config file via an environmental variable, if desired

--- checksummit/__init__.py
+++ checksummit/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 #app.config['DISALLOWED_ALGOS'] = ["md5", "crc32"]
 #app.config['BUFF'] = 1024*1000*8
+app.config.from_envvar('CHECKSUMMIT_CONFIG', silent=True)
 
 
 app.register_blueprint(BLUEPRINT)
